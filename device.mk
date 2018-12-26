e5d7ee300a8038e406c5d08f66b03957a116d983
Ashwin R C <ashwin2001achu@gmail.com>
2018-12-26 14:45:29 +0000

    add gcam-v6 by san1ty

--- device.mk
+++ device.mk
@@ -102,8 +102,10 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    GoogleCameraMod \
     libbson \
     Camera2 \
+    libgui_vendor \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
